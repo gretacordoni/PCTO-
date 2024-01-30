2626e7c7a12c86459bf3dfa56fb72e5d7110ab7a
gretacordoni <158043184+gretacordoni@users.noreply.github.com>
2024-01-30 16:07:25 +0000

    .

---
+++ ex05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int anni;
+
+    printf("inserisci la tua età: ");
+    scanf("%d",&anni);
+    if(anni>=18)
+    {
+        printf("sei maggiorenne ");
+    }
+    else
+    {
+        printf("sei minorenne ");
+    }
+}
